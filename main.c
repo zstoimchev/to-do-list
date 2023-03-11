a82e74fdebd6481e169557f9a52fd3bf04991833
zstoimchev <zivkostoimcev1551@outlook.com>
2023-03-11 12:07:29 +0100

    added icon on exacutable

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-  check_file();  //this needs more more and more work
+  check_file(); // this needs more more and more work
   char cCategory[3] = "x", *options[] = {"Options:\n",
                                          "==========================================================\n",
                                          "a) Mark item as completed ----------------------> type 'a'\n",
@@ -16,17 +16,18 @@ int main()
                                          "==========================================================\n"};
   while (1)
   {
-    clear_screen();
-    printf("Due:\n");
-    display_file("todo.txt");
-    printf("----------------------------------------------------------\n");
-    printf("Completed:\n");
-    display_file("completed.txt");
-    printf("\n");
-    for (int i = 0; i < sizeof(options) / sizeof(options[0]); i++)
-      printf("%s", options[i]);
+
     do
     {
+      clear_screen();
+      printf("Due:\n");
+      display_file("todo.txt");
+      printf("----------------------------------------------------------\n");
+      printf("Completed:\n");
+      display_file("completed.txt");
+      printf("\n");
+      for (int i = 0; i < sizeof(options) / sizeof(options[0]); i++)
+        printf("%s", options[i]);
       printf("Enter your choice: ");
       scanf("%s", cCategory);
     } while (strcmp(cCategory, "a") != 0 && strcmp(cCategory, "b") != 0 && strcmp(cCategory, "c") != 0 && strcmp(cCategory, "d") != 0 && strcmp(cCategory, "q") != 0);
